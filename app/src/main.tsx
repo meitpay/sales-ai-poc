c5813faa660e0fa75df5ffccd32960fdd7b0537e
Mats Haugan <meitpay@gmail.com>
2024-03-19 16:45:16 +0100

    Added react router

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './components/layout/ErrorPage.tsx'
 import App from './App.tsx'
+import { JobListing } from './components/forms/JobListing.tsx'
+import { PeopleSearch } from './components/forms/PeopleSearch.tsx'
+import { MantineProvider } from '@mantine/core'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'job-listing',
+        element: <JobListing />
+      },
+      {
+        path: 'people-search',
+        element: <PeopleSearch />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme='dark'>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>
 )
